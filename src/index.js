a0f14cd930be28464fbc746ae3242f77a5cbeab8
xin <chenxin19920507@gmail.com>
2019-11-07 10:16:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-  if(bol==true){
-  	showtitle();
-  	bol=false;
-  }
+  // if(bol==true){
+  // 	showtitle();
+  // 	bol=false;
+  // }
 }
 showSlides();
 function showtitle(){
